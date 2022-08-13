14c6632b28ede7c63091dd19ae3c5079a06b8611
TesyarRAz <mohamadtesyar@gmail.com>
2022-08-13 11:57:15 +0700

    asd

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -24,7 +24,7 @@
     @stack('css')
 
 	<style type="text/css">
-	.background-style {
+	body {
 		background-repeat: no-repeat;
 		
 		@if ($style->bg_type == 'color')
@@ -39,7 +39,7 @@
 	</style>
 </head>
 <body>
-    <div class="container mt-5" class="background-style">
+    <div class="container my-5">
         <h4 class="text-center">{{ $info->title }}</h4>
         @if(filled($info->image))
         <img src="{{ $info->image }}" alt="image.png" class="img-fluid mx-0">
@@ -54,6 +54,8 @@
     </div>
 
 	{{-- Bootstrap --}}
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
